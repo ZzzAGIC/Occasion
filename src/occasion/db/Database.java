3175080669d15ab1fe7240d4b3314c43d4435165
Rishabh Mathur <rishabhm@usc.edu>
2019-11-09 20:54:45 -0800

    Modified Database

--- src/occasion/db/Database.java
+++ src/occasion/db/Database.java
@@ -9,10 +9,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Database {
-	private Connection conn;
-
-	
-	public Database() throws Exception {
+		
+	private static Connection getConnection() throws SQLException {
 		String databaseName = "NAME";
 		String instanceName = "INSTANCE_NAME";
 		String username = "USERNAME";
@@ -20,15 +18,16 @@ public class Database {
 		String connection_string = String.format("jdbc:mysql://google/%s?cloudSqlInstance=%s&" + "socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false", 
 		        databaseName, instanceName);
 		
-		conn = DriverManager.getConnection(connection_string, username, password);	
+		Connection conn = DriverManager.getConnection(connection_string, username, password);
+		
+		return conn;
 	}
-	
 	/**
 	 * A method that acts as a wrapper for select statements on the database
 	 * @param query - An SQL Select statement
 	 * @return A map consisting of a mapping of column name to an ordered vector of values
 	 */
-	public Map<String, List< Object> > SelectQuery(String query){
+	public static Map<String, List< Object> > SelectQuery(String query){
 		Map<String, List< Object> > result = new HashMap<>();
 		
 		//Note to add prescreening of the query to validate it is indeed a SELECT statement 
@@ -38,7 +37,10 @@ public class Database {
 		
 		Statement st = null;
 		ResultSet rs = null;
+		Connection conn = null;
 		try {				
+			conn = getConnection();
+			
 			st = conn.createStatement();
 				
 			String statement = String.format(query);
@@ -64,6 +66,11 @@ public class Database {
 			} catch(SQLException e) {
 				System.out.println(e);
 			}
+			try {
+				conn.close();
+			} catch(SQLException e) {
+				System.out.println(e);
+			}
 		}
 		return result;
 	}
@@ -72,9 +79,11 @@ public class Database {
 	 * A method that acts as a wrapper for an SQL Statement that has no result set
 	 * @param query - An SQL Remove Query
 	 */
-	public void UpdateQuery(String query) {
+	public static void UpdateQuery(String query) {
 		Statement st = null;
+		Connection conn = null;
 		try {				
+			conn = getConnection();
 			st = conn.createStatement();			
 			String statement = String.format(query);
 			st.executeUpdate(statement);
@@ -86,15 +95,12 @@ public class Database {
 			} catch(SQLException e) {
 				System.out.println(e);
 			}
+			try {
+				conn.close();
+			} catch(SQLException e) {
+				System.out.println(e);
+			}
 		}
-	}
-	
-	/**
-	 * Closes the connection 
-	 * @throws SQLException
-	 */
-	public void Close() throws SQLException {
-		conn.close();
 	}
 }
 
