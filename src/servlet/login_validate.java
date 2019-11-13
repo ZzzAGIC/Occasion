3be795ce93fa5a7b63f0bb90cea76883ecf3e143
Rishabh Mathur <rishabhm@usc.edu>
2019-11-12 21:56:40 -0800

    -

--- src/servlet/login_validate.java
+++ src/servlet/login_validate.java
@@ -36,64 +36,63 @@ public class login_validate extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-			String login_name= request.getParameter("username");
-			String login_pass= request.getParameter("userpass");
+		String login_name= request.getParameter("username");
+		String login_pass= request.getParameter("userpass");
 			
-			String next = "/HomePage.jsp";
-			String error = "";
-			HttpSession session = request.getSession();
-			session.setAttribute("login", false);
+		String next = "/HomePage.jsp";
+		String error = "";
+		HttpSession session = request.getSession();
+		session.setAttribute("login", false);
 			
 
-			if(login_name =="") {
-				error += "Username cannot be empty & ";
-				next = "/Login.jsp";
-			}
-			if(login_pass =="") {
-				error += "password cannot be empty & ";
-				next = "/Login.jsp";
-			}
-			try {
-				//login success
-				if (validateCredentials(login_name, login_pass)) {
-					session.setAttribute("login", true);
-					session.setAttribute("myname", login_name);
-				}
-				//login failed
-				else {
-					error += "invalid username or wrong password  & ";
-					next = "/Login.jsp";
-				}
-			} catch (SQLException e){
-				e.printStackTrace();
-			} catch (ClassNotFoundException e){
-				e.printStackTrace();
-			}
-			
-			
-			
-			request.setAttribute("error", error);
-			RequestDispatcher dispatch = getServletContext().getRequestDispatcher(next);
-			
-			try {
-				dispatch.forward(request,response);
-			}
-			catch (IOException e){
-				e.printStackTrace();
-			}
-			catch (ServletException e){
-				e.printStackTrace();
-			}
-	    }
-	    
-		public static boolean validateCredentials(String username, String password) throws SQLException, ClassNotFoundException {
-			String query = "SELECT * FROM User WHERE Username = ? AND Password = ?";
-			List<List<String>> output = Database.SelectQuery(query, username, password);
-			
-			if(output.size() > 0) return true;
-			
-			return false;
+		if(login_name =="") {
+			error += "Username cannot be empty & ";
+			next = "/Login.jsp";
 		}
+		if(login_pass =="") {
+			error += "password cannot be empty & ";
+			next = "/Login.jsp";
+		}
+		try {
+			//login success
+			if (validateCredentials(login_name, login_pass)) {
+				session.setAttribute("login", true);
+				session.setAttribute("myname", login_name);
+			}
+			//login failed
+			else {
+				error += "invalid username or wrong password  & ";
+				next = "/Login.jsp";
+			}
+		} catch (SQLException e){
+			e.printStackTrace();
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
+			
+				
+		request.setAttribute("error", error);
+		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(next);
+			
+		try {
+			dispatch.forward(request,response);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		catch (ServletException e){
+			e.printStackTrace();
+		}
+	}
+	    
+	public static boolean validateCredentials(String username, String password) throws SQLException, ClassNotFoundException {
+		String query = "SELECT * FROM User WHERE Username = ? AND Password = ?";
+		List<List<String>> output = Database.SelectQuery(query, username, password);
+			
+		if(output.size() > 0) return true;
+			
+		return false;
+	}
 			
 
 }
